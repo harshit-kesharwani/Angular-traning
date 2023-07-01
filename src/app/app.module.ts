0dd96ed9fb67aacf71903cf7061bb3d02af4105e
Harshit <60813225+harshit-kesharwani@users.noreply.github.com>
2023-07-01 09:51:47 +0530

    This is the Angular Project done so far.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,23 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UserinfoformComponent } from './userinfoform/userinfoform.component';
+import {FormsModule} from "@angular/forms";
+import { EmailformComponent } from './emailform/emailform.component';
+import {HttpClientModule} from "@angular/common/http";
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserinfoformComponent,
+    EmailformComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/userinfo.ts
@@ -0,0 +1,11 @@
+export class Userinfo {
+
+  constructor(
+    public username: string,
+    public fname: string,
+    public email: string,
+    public phone: string,
+    public lname: string,
+  ) {  }
+
+}
